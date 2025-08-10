9404e53daa436a6e57c92c9ff8739b43d0a47227
Daniel Oliveira <contatodanielaso@gmail.com>
2025-08-10 02:32:20 -0300

    Razão Sexo 2010 e 2022

--- Demografia_censo.R
+++ Demografia_censo.R
@@ -2,16 +2,15 @@
 library(sidrar)
 #install.packages("dplyr")
 library(dplyr)
+#install.packages("gtsummary")
+library(gtsummary)
 
 info_sidra(1209)
-
-# Códigos das tabelas SIDRA
-tab_censos <- c(200, 1209, 9514)
 
 # Código do Acre é 12
 cod_uf_acre <- 12
 
-# Para cada tabela, buscar dados de 2010 e 2022 para o Acre
+# Buscar dados de 2022 para o Acre
 acre_idade_2022 <-   get_sidra(
     x = 9514,
     period = "2022", # anos do censo
@@ -20,7 +19,20 @@ acre_idade_2022 <-   get_sidra(
   )
 
 acre_idade_filtrado <- acre_idade_2022 %>%
-  select(-contains("(Código)"))
+  select(-contains("(Código)")) %>%
+  select(-"Nível Territorial", -"Unidade de Medida") %>%
+  filter(!grepl("Total", Sexo))
+
 
 # Olhar os dados da primeira tabela como exemplo
 str(acre_idade_filtrado)
+
+info_sidra(3145)
+
+# Buscar dados de 2010 para o Acre
+acre_idade_2010 <-   get_sidra(
+    x = 1378,
+    period = "2010", # ano do censo
+    geo = "State",
+    geo.filter = list(State = cod_uf_acre)
+  )
